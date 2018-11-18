fa3dbb7adae666d22baaadc0075fc22a0cfd150f
missKsu <ks.evdokimova.v@gmail.com>
2018-11-18 14:23:30 +0300

    SocialNetwork/scripts/linux/test.sh - run all tests

--- scripts/linux/test.sh
+++ scripts/linux/test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 dotnet test ${TRAVIS_BUILD_DIR}/tests/Users.Test
+dotnet test ${TRAVIS_BUILD_DIR}/tests/Permissions.Test
+dotnet test ${TRAVIS_BUILD_DIR}/tests/Groups.Test
+dotnet test ${TRAVIS_BUILD_DIR}/tests/Posts.Test
+dotnet test ${TRAVIS_BUILD_DIR}/tests/SocialNetwork.Test
