7eaa5554e3faad021c9d4b83045772e2e2663740
missKsu <ks.evdokimova.v@gmail.com>
2018-12-11 03:32:38 +0300

    SocialNetwork\SocialNetwork\Api\PostsApi.cs - add

---
+++ SocialNetwork/Api/PostsApi.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Posts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Api
+{
+    public class PostsApi : BaseApi
+    {
+        private string address;
+
+        public PostsApi(IConfiguration configuration)
+        {
+            address = configuration.GetSection("Addresses")["Posts"];
+        }
+
+        public PostsApi()
+        {
+
+        }
+        
+        public virtual (List<Post>,int) GetPostsByAuthor(int name, int page, int perpage)
+        {
+            var groupResponse = GetRequest($"{address}posts/author/{name}?page={page}&perpage={perpage}");
+            string jsonResponse = groupResponse.Content.ReadAsStringAsync().Result;
+            var posts = JsonConvert.DeserializeObject<(List<Post>,int)>(jsonResponse);
+            return posts;
+        }
+
+        public virtual (List<Post>, int) GetPostsByGroup(int name, int page, int perpage)
+        {
+            var groupResponse = GetRequest($"{address}posts/group/{name}?page={page}&perpage={perpage}");
+            string jsonResponse = groupResponse.Content.ReadAsStringAsync().Result;
+            var posts = JsonConvert.DeserializeObject<(List<Post>, int)>(jsonResponse);
+            return posts;
+        }
+    }
+}
