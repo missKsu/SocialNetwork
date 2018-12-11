704f3e2404323bbec359cf94cd9a40ede2de56b7
missKsu <ks.evdokimova.v@gmail.com>
2018-12-11 03:32:07 +0300

    SocialNetwork\SocialNetwork\Api\PermissionsApi.cs - add

---
+++ SocialNetwork/Api/PermissionsApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Api
+{
+    public class PermissionsApi : BaseApi
+    {
+    }
+}
