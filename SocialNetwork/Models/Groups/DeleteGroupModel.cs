192acb378b03c2e25c4165a766de2f840268c4f1
missKsu <ks.evdokimova.v@gmail.com>
2018-12-12 14:42:44 +0300

    SocialNetwork\SocialNetwork\Models\Groups\DeleteGroupModel.cs - add

---
+++ SocialNetwork/Models/Groups/DeleteGroupModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.Groups
+{
+    public class DeleteGroupModel
+    {
+        public string GroupName { get; set; }
+        public string Creator { get; set; }
+    }
+}
