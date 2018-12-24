3300da6993acdd099aa9278545f7795eb9d88be5
missKsu <ks.evdokimova.v@gmail.com>
2018-12-24 23:44:59 +0300

    SocialNetwork\SocialNetwork\Views\Gateway\DeletePostIf.cs -add

---
+++ SocialNetwork/Views/Gateway/DeletePostIf.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialNetwork.Views.Gateway
+{
+    public class DeletePostIfModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
