7cb4bfcf695863ad516c85204ec1eb560f91d7b6
missKsu <ks.evdokimova.v@gmail.com>
2018-12-24 23:46:39 +0300

    SocialNetwork\SocialNetwork\Models\Permissions\AllPermissionsForUser.cs - add

---
+++ SocialNetwork/Models/Permissions/AllPermissionsForUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.Permissions
+{
+    public class AllPermissionsForUser
+    {
+        public List<PermissionModel> UserPermissions { get; set; }
+        public List<PermissionModel> EditablePermissions { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
