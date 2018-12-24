3ec0aefb4f33adeb2362540aaf708209fa3c1895
missKsu <ks.evdokimova.v@gmail.com>
2018-12-25 00:08:02 +0300

    SocialNetwork\SocialNetwork\Models\Posts\DeletePostModel.cs - add

---
+++ SocialNetwork/Models/Posts/DeletePostModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.Posts
+{
+    public class DeletePostModel
+    {
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string Creator { get; set; }
+    }
+}
