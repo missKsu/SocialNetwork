48fe3d4d25bcc6f38b37801699c9b12a1b1512be
missKsu <ks.evdokimova.v@gmail.com>
2018-12-24 23:50:16 +0300

    SocialNetwork\SocialNetwork\Models\Permissions\AllPermissionsModel.cs - add

---
+++ SocialNetwork/Models/Permissions/AllPermissionsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.Permissions
+{
+    public class AllPermissionsModel
+    {
+        public List<PermissionModel> Permissions { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
