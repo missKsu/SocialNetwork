4f8321796a343b5c3750a13a14e8f65b79f68935
missKsu <ks.evdokimova.v@gmail.com>
2018-12-25 00:09:05 +0300

    SocialNetwork\SocialNetwork\Models\Posts\EditPostModel.cs - add

---
+++ SocialNetwork/Models/Posts/EditPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models.Posts
+{
+    public class EditPostModel
+    {
+        public int Id { get; set; }
+        public string Creator { get; set; }
+        public string Group { get; set; }
+        public string Text { get; set; }
+        public string NewText { get; set; }
+    }
+}
